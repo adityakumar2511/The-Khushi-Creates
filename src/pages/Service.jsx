d2064527cf53f4bdfb02fff97e70b202a0ae3550
adityakumar2511 <AdityaKumar@123>
2026-02-15 21:19:57 +0530

    add

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,10 +1,12 @@
 import ServiceHeader from "../components/ServiceHeader";
 import ServiceSection from "../components/ServiceSection";
+import CTA from "../components/CTA";
 export default function Service() {
   return (
     <div>
       <ServiceHeader />
       <ServiceSection />
+      <CTA />
     </div>
   );
 }
